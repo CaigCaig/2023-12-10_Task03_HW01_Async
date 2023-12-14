3711c5c1395b3d119b82e632806086fa843de460
CaigCaig <caig@inbox.ru>
2023-12-14 23:19:31 +0300

    2023-12-14 Correction

--- 2023-12-10_Task03_HW01_Async.cpp
+++ 2023-12-10_Task03_HW01_Async.cpp
@@ -8,50 +8,7 @@ using namespace std;
 
 constexpr int size_v = 20;
 
-#define NO_PROMISE
-
-#ifdef NO_PROMISE
-int search_min(vector<int> data, int i)
-{
-	auto val = i;
-	for (auto j = i + 1; j < data.size(); j++) if (data[j] < data[val]) val = j;
-	return val;
-}
-
-vector<int> selection_sort(vector<int> arr, int size)
-{
-	for (auto i = 0; i < size; ++i)
-	{
-		auto i_swap = async(search_min, arr, i);
-		swap(arr[i], arr[i_swap.get()]);
-	}
-	return arr;
-}
-
-int main()
-{
-	vector<int> data(size_v);
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<int> rnd(0, 100);
-	for (auto i = 0; i < size_v; ++i)
-	{
-		data[i] = rnd(rd);
-		cout << data[i] << ' ';
-	}
-	cout << endl;
-
-	auto sort_data = async(selection_sort, data, size_v);
-	auto vec = sort_data.get();
-
-	for (auto i = 0; i < size_v; ++i) cout << vec[i] << ' ';
-	cout << endl;
-
-	system("pause");
-}
-
-#else
-int search_min(vector<int> data, int i, promise<int>prom)
+void search_min(vector<int> data, int i, promise<int>prom)
 {
 	auto val = i;
 	for (auto j = i + 1; j < data.size(); j++) if (data[j] < data[val]) val = j;
@@ -60,11 +17,11 @@ int search_min(vector<int> data, int i, promise<int>prom)
 
 vector<int> selection_sort(vector<int> arr, int size)
 {
-	promise<int>prom;
-	future<int> i_swap = prom.get_future();
 	for (auto i = 0; i < size; ++i)
 	{
-		auto ft = async(search_min, arr, i, move(prom));
+		promise<int>prom;
+		future<int> i_swap = prom.get_future();
+		async(search_min, arr, i, move(prom));
 		swap(arr[i], arr[i_swap.get()]);
 	}
 	return arr;
@@ -91,7 +48,6 @@ int main()
 
 	system("pause");
 }
-#endif
 
 /*
 Сортировка выбором
